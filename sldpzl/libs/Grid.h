bff2529f5a1182f48c4e39a7588e0b1c03a12874
sagara-pathum <sagarapathum0618@gmail.com>
2026-07-21 18:16:22 +0530

    project made structure, build files added to the gitignore

---
+++ sldpzl/libs/Grid.h
@@ -0,0 +1,32 @@
+#ifndef GRID_H
+#define GRID_H
+
+#include "../libs/Pzl.h" // Pzl.h has #include <vector> so i am not including it here
+
+class Grid : public Pzl
+{
+	private :
+		std::vector< std::vector<int> >* v_ptr = &v;
+		// i have to use those in tow functions so i made them local varibles for this class.
+	    int n_rows;
+	    int n_columns;
+	    int row_jumps;
+	    int col_jumps;          
+	    int x_start;
+	    int y_start;
+
+	public :
+
+		Grid (int grid_size);
+
+		void print_grid(); // print_grid function prints 3 by 3 grid on screen
+
+		void add_vec(); // this is the function that add the vec_values of Pzl class on the drawn Grid
+
+		void mvprintw(int x, int y, int n); // add_vec is a set of individual prints of numbers. those individuals n  is printed on x, y using this function
+
+		void gexec(); // this is the evenloop that holds and updates the TUI grid interface.
+
+};
+
+#endif
---
+++ sldpzl/modules/Grid.cpp
@@ -0,0 +1,92 @@
+#include "../libs/Grid.h"
+
+Grid::Grid (int grid_size){
+	
+	n_rows = grid_size;
+	n_columns = grid_size*2;
+	row_jumps = n_rows/3;
+	col_jumps = n_columns/3;
+
+	x_start = (max_x - n_columns)/2;
+	y_start = (max_y - n_rows)/2;
+	
+}
+
+void Grid::mvprintw(int x, int y, int n){
+	move(y, x);
+	if (n != 0) {
+		printw("%d ",n);
+	} else {
+		printw(" ");
+	}
+}
+
+void Grid::add_vec(){
+    int x1 = x_start + (col_jumps/2);
+    int x2 = x_start + 3*(col_jumps/2);
+	int x3 = x_start + 5*(col_jumps/2);
+
+	int y1 = y_start + (row_jumps/2);
+	int y2 = y_start + 3*(row_jumps/2);
+	int y3 = y_start + 5*(row_jumps/2);
+
+	mvprintw(x1, y1, (*v_ptr)[0][0]);
+	mvprintw(x2, y1, (*v_ptr)[0][1]);
+	mvprintw(x3, y1, (*v_ptr)[0][2]);
+
+	mvprintw(x1, y2, (*v_ptr)[1][0]);
+	mvprintw(x2, y2, (*v_ptr)[1][1]);
+	mvprintw(x3, y2, (*v_ptr)[1][2]);
+
+	mvprintw(x1, y3, (*v_ptr)[2][0]);
+	mvprintw(x2, y3, (*v_ptr)[2][1]);
+	mvprintw(x3, y3, (*v_ptr)[2][2]);
+
+}
+
+void Grid::print_grid(){
+
+
+    curs_set(0); // Hide the blinking cursor
+	
+    // draw the horizontal lines
+    for (int i = 0; i <= n_rows; i += row_jumps){
+    	// ASC_HLINE is an unicode character we use to draw horizontal lines 
+        // para : y, x, char, lonng 
+        mvhline(y_start + i, x_start, ACS_HLINE, n_columns);
+    }
+    // draw the vertical lines
+    for (int j = 0; j <= n_columns; j += col_jumps){
+        mvvline(y_start, x_start + j, ACS_VLINE, n_rows);
+    }
+                  
+    // we can smooth the corners by uing Alternative Character Set CORNERS
+    // para : y , x, char
+    mvaddch(y_start, x_start, ACS_ULCORNER);
+    mvaddch(y_start, (x_start + n_columns), ACS_URCORNER);
+    mvaddch((y_start + n_rows), x_start, ACS_LLCORNER);
+    mvaddch((y_start + n_rows), (x_start + n_columns), ACS_LRCORNER);
+
+    // now we can add ACS Tees for smooothing the middle lines on edges
+    for (int i = col_jumps; i < n_columns; i += col_jumps){
+        mvaddch(y_start, (x_start + i), ACS_TTEE); // TOP TEE
+        mvaddch((y_start + n_rows), (x_start + i), ACS_BTEE); // BOTOM TEE
+    }
+
+    for (int j = row_jumps; j < n_rows; j += row_jumps){
+        mvaddch((y_start + j), x_start, ACS_LTEE); //LEFT TEE
+        mvaddch((y_start + j), (x_start + n_columns), ACS_RTEE); //RIGHT TEE
+    }
+}
+
+void Grid::gexec(){
+    int ch;
+	print_grid();
+	add_vec();
+	refresh();
+	while((ch=getch()) != 'q'){					
+		move_tile(ch);
+		add_vec();
+        refresh();
+	}	
+}
